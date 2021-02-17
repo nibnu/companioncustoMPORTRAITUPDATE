0ced2e3a9009dbaf5473a7a6df9fb9a05848970e
nibnu <93439613+nibnu@users.noreply.github.com>
2021-02-17 13:08:24 +0000

    Create Contributing.md

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,8 @@
 //! Collection of many various encrypting functions implementations
 //! in pure Rust. Every contained encryption method has its own module
 //! with functions for encrypting and decrypting appropriate type of message.
+
+pub mod ceasar;
+pub mod substitution;
+
+fn check_message_from_letters(message: &[u8]) {
