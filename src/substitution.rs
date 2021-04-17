f84382a6fd3836c1866205491520558fb3076a1f
nibnu <93439613+nibnu@users.noreply.github.com>
2021-04-17 16:09:58 +0000

    Update Package.swift

--- src/substitution.rs
+++ src/substitution.rs
@@ -3,4 +3,4 @@
 use crate::check_message_from_letters;
 
 /// Encrypts `message` by substitution according to `permutation`.
-/// Every letter is substituted for letter at position (letter's 
+/// Every letter is substituted for letter at position (letter's alphabe
