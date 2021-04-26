3cdedf4558ca37597505605d101043d28ff17dee
nibnu <93439613+nibnu@users.noreply.github.com>
2021-04-26 16:16:30 +0000

    1.12

--- src/substitution.rs
+++ src/substitution.rs
@@ -4,4 +4,4 @@ use crate::check_message_from_letters;
 
 /// Encrypts `message` by substitution according to `permutation`.
 /// Every letter is substituted for letter at position (letter's alphabet index)
-/// in `permutat
+/// in `permutation` 
