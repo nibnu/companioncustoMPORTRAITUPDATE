5ecbba957c663668c86780e5a31fb2df699332d3
nibnu <93439613+nibnu@users.noreply.github.com>
2021-05-13 07:02:45 +0000

    trest

--- src/substitution.rs
+++ src/substitution.rs
@@ -7,4 +7,4 @@ use crate::check_message_from_letters;
 /// in `permutation` array.
 ///
 /// # Panics
-/// Panics if `permutation` is not a valid ASCII 
+/// Panics if `permutation` is not a valid ASCII lowerca
