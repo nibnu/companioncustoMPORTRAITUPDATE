7117d85c5ae37573640db484c1e7a8da70d5efb4
nibnu <93439613+nibnu@users.noreply.github.com>
2021-05-24 22:41:20 +0000

    save progress

--- src/substitution.rs
+++ src/substitution.rs
@@ -9,4 +9,5 @@ use crate::check_message_from_letters;
 /// # Panics
 /// Panics if `permutation` is not a valid ASCII lowercase permutation or
 /// `message` doesn't consist of ASCII lowercase letters.
+///
 //
