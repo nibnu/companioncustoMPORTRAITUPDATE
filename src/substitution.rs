dde6df2a00f44defbcee10fbfd38e6c55e97e41b
nibnu <93439613+nibnu@users.noreply.github.com>
2021-07-02 07:48:55 +0000

    added modules

--- src/substitution.rs
+++ src/substitution.rs
@@ -16,4 +16,4 @@ use crate::check_message_from_letters;
 ///
 /// let message = b"hello";
 /// let permutation = b"zyxwvutsrqponmlkjihgfedcba";
-/// let encrypted_message = substitution_c
+/// let encrypted_message = substitution_cipher
