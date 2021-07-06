30d3b2127b03e7bca2d9affb43f42340e262b2f2
nibnu <93439613+nibnu@users.noreply.github.com>
2021-07-06 05:39:32 +0000

    self

--- src/substitution.rs
+++ src/substitution.rs
@@ -16,4 +16,6 @@ use crate::check_message_from_letters;
 ///
 /// let message = b"hello";
 /// let permutation = b"zyxwvutsrqponmlkjihgfedcba";
-/// let encrypted_message = substitution_cipher(message, *permutatio
+/// let encrypted_message = substitution_cipher(message, *permutation);
+
+/
