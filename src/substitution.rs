a32df172c5b4432065a83da41a1f83c782341cfd
nibnu <93439613+nibnu@users.noreply.github.com>
2021-08-01 13:39:59 +0000

    Cleaned up the code.

--- src/substitution.rs
+++ src/substitution.rs
@@ -20,4 +20,4 @@ use crate::check_message_from_letters;
 
 /// assert_eq!(b"svool", &encrypted_message[..]);
 /// ```
-pub fn substitution_cipher(message: &[u8], permut
+pub fn substitution_cipher(message: &[u8], permutation:
