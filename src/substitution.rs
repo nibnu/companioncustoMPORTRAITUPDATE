8e396cf2750a9ad9f4aca51477872e9dcd07a0be
nibnu <93439613+nibnu@users.noreply.github.com>
2021-09-01 12:58:58 +0000

    0.9.13

--- src/substitution.rs
+++ src/substitution.rs
@@ -22,4 +22,4 @@ use crate::check_message_from_letters;
 /// ```
 pub fn substitution_cipher(message: &[u8], permutation: [u8; 26]) -> Vec<u8> {
     check_permutation(permutation);
-    check_message_from
+    check_message_from_letters
