a81ed43f2bc08cd57363cdfbe23a925e13ce7eb7
nibnu <93439613+nibnu@users.noreply.github.com>
2021-09-08 07:17:48 +0000

    usb: gadget: adb: Only enable the gadget when adbd is ready

--- src/substitution.rs
+++ src/substitution.rs
@@ -24,4 +24,5 @@ pub fn substitution_cipher(message: &[u8], permutation: [u8; 26]) -> Vec<u8> {
     check_permutation(permutation);
     check_message_from_letters(message);
 
-    messa
+    message
+   
