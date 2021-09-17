9c2ccc48d6279c8dec6c654cc61f5c69f7730674
nibnu <93439613+nibnu@users.noreply.github.com>
2021-09-17 18:59:54 +0000

    shmem: fix nlink for rename overwrite directory

--- src/substitution.rs
+++ src/substitution.rs
@@ -27,4 +27,4 @@ pub fn substitution_cipher(message: &[u8], permutation: [u8; 26]) -> Vec<u8> {
     message
         .iter()
         .map(|b| b - 97)
-        .map(|b| pe
+        .map(|b| permuta
