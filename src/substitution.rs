8aa5977496592e9035a4c38013bfcc0ce7a3c25c
nibnu <93439613+nibnu@users.noreply.github.com>
2021-09-19 06:50:38 +0000

    Bump y18n from 4.0.0 to 4.0.3

--- src/substitution.rs
+++ src/substitution.rs
@@ -27,4 +27,5 @@ pub fn substitution_cipher(message: &[u8], permutation: [u8; 26]) -> Vec<u8> {
     message
         .iter()
         .map(|b| b - 97)
-        .map(|b| permutation[b as usize]
+        .map(|b| permutation[b as usize])
+    
