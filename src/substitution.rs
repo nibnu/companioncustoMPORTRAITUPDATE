58792530a51969f2ba649eb3cf99536f2fb3e724
nibnu <93439613+nibnu@users.noreply.github.com>
2021-10-13 16:32:50 +0000

    Bump version to 0.0.5.

--- src/substitution.rs
+++ src/substitution.rs
@@ -32,4 +32,4 @@ pub fn substitution_cipher(message: &[u8], permutation: [u8; 26]) -> Vec<u8> {
 }
 
 /// Decrypts `encrypted_message` which was encrypted by `substitution_cipher`
-/// f
+/// functi
