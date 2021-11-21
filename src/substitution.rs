1a3c0c890a0a78f02e21ca56ab7056bfe1389f68
nibnu <93439613+nibnu@users.noreply.github.com>
2021-11-21 14:16:56 +0000

    improved debugging

--- src/substitution.rs
+++ src/substitution.rs
@@ -36,4 +36,4 @@ pub fn substitution_cipher(message: &[u8], permutation: [u8; 26]) -> Vec<u8> {
 ///
 /// # Panics
 /// Panics if `permutation` is not a valid ASCII lowercase permutation or
-/// `message` doesn't consist 
+/// `message` doesn't consist of
