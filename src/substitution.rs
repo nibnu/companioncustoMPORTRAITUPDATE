ae649c8fe4b1285250d17a39cb8806df10f3d7c3
nibnu <93439613+nibnu@users.noreply.github.com>
2022-10-16 21:14:42 +0000

    Strip whitespace

--- src/substitution.rs
+++ src/substitution.rs
@@ -78,4 +78,4 @@ fn check_permutation(permutation: [u8; 26]) {
 
     for letter in permutation {
         println!("{}", letter);
-        assert!(letter >= 97 &
+        assert!(letter >= 97 && lette
