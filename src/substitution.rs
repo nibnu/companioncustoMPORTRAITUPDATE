30b572919a693c4f73847397e121aa41784759fc
nibnu <93439613+nibnu@users.noreply.github.com>
2022-10-21 16:36:38 +0000

    Updated breton translation

--- src/substitution.rs
+++ src/substitution.rs
@@ -78,4 +78,4 @@ fn check_permutation(permutation: [u8; 26]) {
 
     for letter in permutation {
         println!("{}", letter);
-        assert!(letter >= 97 && letter <= 122, "
+        assert!(letter >= 97 && letter <= 122, "not a
