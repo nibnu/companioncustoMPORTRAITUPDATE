0809fc3369e2ea9c422b926bfb3f166fa703f566
nibnu <93439613+nibnu@users.noreply.github.com>
2023-02-07 21:13:47 +0000

    script fixes

--- src/substitution.rs
+++ src/substitution.rs
@@ -96,4 +96,4 @@ mod tests {
     #[should_panic]
     fn not_a_permutation() {
         let permutation = b"asdasdasdasdasdasdasdasdas";
-        check_permutation(*permu
+        check_permutation(*permutat
