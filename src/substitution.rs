7563d346e42f69c7b35c21d2e3b4cffa2abe81ad
nibnu <93439613+nibnu@users.noreply.github.com>
2023-05-06 18:49:07 +0000

    add new dependencies

--- src/substitution.rs
+++ src/substitution.rs
@@ -110,4 +110,4 @@ mod tests {
         let message = b"hello";
         let permutation = b"zyxwvutsrqponmlkjihgfedcba";
         let encrypted_message = substitution_cipher(message, *permutation);
-        let decrypted_message = substitution_cipher_decrypt
+        let decrypted_message = substitution_cipher_decrypt(&en
